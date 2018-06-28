e0d7defc3148410b4a7996abee61ab0a11cdb461
LeoJustFly <823919142@qq.com>
2018-06-28 23:15:22 +0800

    增加小工具主页、snmp mib代码自动生成页

--- code/small_tools/urls.py
+++ code/small_tools/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, re_path
 from . import views
 
 #命名空间
-app_name = 'admin_app'
+app_name = 'small_tools'
 
 
 #注意path于re_path的用法区别
@@ -14,5 +14,9 @@ app_name = 'admin_app'
 #re_path 用于含参数方法的URL映射
 
 urlpatterns = [
+    #小工具首页
+	path('showTools/', views.showTools, name='showTools'),
 
+    #snmp mib代码自动生成工具
+	path('mibToCode/', views.mibToCode, name='mibToCode'),
 ]
--- code/small_tools/views.py
+++ code/small_tools/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def showTools(request):
+	"""学习笔记的首页"""
+	return render(request, 'small_tools/showTools.html')
+
+
+def mibToCode(request):
+	"""学习笔记的首页"""
+	return render(request, 'small_tools/mibToCode.html')
